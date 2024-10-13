86091cdf46d985f3668a02983d74d79035f934f4
berserks03 <berserks@inbox.lv>
2024-10-14 02:23:11 +0300

    fix url

--- app/utils/fetchArtObject.ts
+++ app/utils/fetchArtObject.ts
@@ -1,12 +1,18 @@
+import { headers } from 'next/headers';
+
 export const fetchArtObject = async (id: string) => {
-    
-    const response = await fetch(`http://localhost:3000/api/getArtObject?id=${id}`);
+    const headersList = headers();
+    const host = headersList.get('host');
+    const protocol = process.env.NODE_ENV === 'development' ? 'http' : 'https';
+    const url = `${protocol}://${host}`;
+
+    const response = await fetch(`${url}/api/getArtObject?id=${id}`);
 
     if (!response.ok) {
         return null;
     }
 
     const data = await response.json();
-    
+
     return data.data;
 };
